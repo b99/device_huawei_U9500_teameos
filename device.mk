15223d030c951a72d6f02b7e93dc53fcbefdee7c
ShevT <shev_t@mail.ru>
2015-11-13 09:30:41 +0300

    front_audio_config.conf -> eos_front_audio_config.conf

--- device.mk
+++ device.mk
@@ -66,8 +66,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_FOLDER)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(COMMON_FOLDER)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(COMMON_FOLDER)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(COMMON_FOLDER)/configs/audio/cm_front_audio_config.conf:system/etc/huawei/audio/eos_front_audio_config.conf \
-    $(COMMON_FOLDER)/configs/audio/cm_front_audio_config.conf:system/etc/huawei/audio/aosp_front_audio_config.conf \
+    $(COMMON_FOLDER)/configs/audio/front_audio_config.conf:system/etc/huawei/audio/eos_front_audio_config.conf \
+    $(COMMON_FOLDER)/configs/audio/front_audio_config.conf:system/etc/huawei/audio/aosp_front_audio_config.conf \
     $(COMMON_FOLDER)/configs/audio/front_audio_config.conf:system/etc/huawei/audio/front_audio_config.conf \
     $(COMMON_FOLDER)/configs/audio/front_factory_audio_config.conf:system/etc/huawei/audio/front_factory_audio_config.conf
 
